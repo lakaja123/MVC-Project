5986ea3ab8ce665257731f0e026216b63b09aaec
lakaja123 <grealm@bk.ru>
2022-03-23 12:41:30 +0500

    version 1.0.8

--- core/Controllers/PostController.php
+++ core/Controllers/PostController.php
@@ -2,7 +2,50 @@
 
 namespace Controllers;
 
+use Models\Post;
+
 class PostController extends BaseController
 {
+    public function create()
+    {
+        return view('posts/create');
+    }
 
+    public function createPost()
+    {
+        # Если пользователь не авторизован!
+        if(!has_session('id'))
+            return redirect('/');
+
+        # Default значение переменной $errors
+        $errors = [];
+
+        # Проверка на существование
+        if(!isset($_POST['name'])) $errors['name'][] = 'Поле не существует!';
+        if(!isset($_POST['keywords'])) $errors['keywords'][] = 'Поле не существует!';
+        if(!isset($_POST['description'])) $errors['description'][] = 'Поле не существует!';
+
+        # Проверка на заполненность
+        if(empty($_POST['name'])) $errors['name'][] = 'Поле не заполнено!';
+        if(empty($_POST['keywords'])) $errors['keywords'][] = 'Поле не заполнено!';
+        if(empty($_POST['description'])) $errors['description'][] = 'Поле не заполнено!';
+
+        # Если существуют ошибки, отправляем их на клиента
+        if($errors != [])
+            return view('posts/create', compact('errors'));
+
+        # Помещаем массив суперглобальной переменной $_POST в переменную $inputs
+        $inputs = $_POST;
+        # Добавляем не достающий элемент пользовательского ID которому он принадлежит
+        $inputs['user_id'] = session('id');
+        # Добавляем не достающий элемент сегодняшней даты и времени
+        $inputs['created_at'] = date('d-m-Y H:i:s');
+
+        # Создаем новый пост
+        $post = new Post();
+        $post->create($inputs);
+
+        # Отправляем представление posts/create со значением выполнения операции успешно
+        return view('posts/create', ['success' => true]);
+    }
 }
--- core/Models/Post.php
+++ core/Models/Post.php
@@ -4,4 +4,6 @@ namespace Models;
 
 class Post extends Model
 {
+    # Присваиваем название таблицы с которой работает модель
+    protected $table = 'posts';
 }
